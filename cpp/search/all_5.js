ad1fbc8d4f3e5e8452544da01bf9e89e3cbcb2f7
TB5zhh <tianbeiwen88@gmail.com>
2024-05-31 00:50:53 +0800

    Removed everything with mike 2.1.1

--- cpp/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update_5fmotor_5f_42',['update_motor_',['../classarm_1_1Robot.html#addd37390481bde902f265a0b6a726258',1,'arm::Robot']]]
-];
--- cpp/search/functions_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['add_5ftarget_5fjoint_5fq_48',['add_target_joint_q',['../classarm_1_1Robot.html#a052b319a1fd43a848d958b1fb5bd7272',1,'arm::Robot']]],
-  ['add_5ftarget_5fjoint_5fv_49',['add_target_joint_v',['../classarm_1_1Robot.html#a347d7da27770aca589cf88e880b97548',1,'arm::Robot']]],
-  ['add_5ftarget_5frelative_5frotation_50',['add_target_relative_rotation',['../classarm_1_1Robot.html#a82866fabc682085a66f148048eea4bf3',1,'arm::Robot']]],
-  ['add_5ftarget_5frelative_5ftranslation_51',['add_target_relative_translation',['../classarm_1_1Robot.html#ab8d508d33cc5e4c0092d0714219702e5',1,'arm::Robot']]],
-  ['add_5ftarget_5ftranslation_52',['add_target_translation',['../classarm_1_1Robot.html#ad3478cb578e4722f0d32ca07e13cb384',1,'arm::Robot']]]
-];
