ec28fbc4567596d856aced463bf41645efaa0e47
TB5zhh <tianbeiwen88@gmail.com>
2024-06-13 18:15:29 +0800

    Deployed a869592 to latest with MkDocs 1.6.0 and mike 2.1.1

--- latest/cpp/search/all_6.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['offline_5fmode_0',['offline_mode',['../classarm_1_1Robot.html#a0909c8ab84325abbeb1aa196115fa3f6',1,'arm::Robot']]],
-  ['online_5fmode_1',['online_mode',['../classarm_1_1Robot.html#a0e104211c880b0038dca59a573cd3529',1,'arm::Robot']]]
-];
